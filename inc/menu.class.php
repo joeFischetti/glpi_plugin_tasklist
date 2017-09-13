69d66c81d27770736554acdd6e2fc29ac9c7a1ab
Joe Fischetti <jkfischetti@gmail.com>
2017-09-13 14:48:42 -0400

    Update menu.class.php

--- inc/menu.class.php
+++ inc/menu.class.php
@@ -1,30 +1,27 @@
 <?php
 class PluginTasklistMenu extends CommonGLPI {
-   static $rightname = 'entity';
+	static $rightname = 'entity';
 
-   static function getMenuName() {
+   	static function getMenuName() {
+		return __("Tasklist List Management", "tasklist");
+	}
 
-	return __("Tasklist List Management", "tasklist");
-   }
+   	static function getMenuContent() {
 
-   static function getMenuContent() {
-
-      	if (!Session::haveRight('entity', READ)) {
+      		if (!Session::haveRight('entity', READ)) {
         	return;
-      	}
+      		}
 
 
-      	$front_tasklist = "/plugins/tasklist/front";
-      	$menu = array();
-      	$menu['title'] = self::getMenuName();
-      	$menu['page']  = "$front_tasklist/list.php";
+      		$front_tasklist = "/plugins/tasklist/front";
+      		$menu = array();
+      		$menu['title'] = self::getMenuName();
+      		$menu['page']  = "$front_tasklist/list.php";
 
-      	$menu['links']['search'] = "/plugins/tasklist/front/list.php";
-      
-        $menu['links']['add'] = "/plugins/tasklist/front/list.form.php";
+	      	$menu['links']['search'] = "/plugins/tasklist/front/list.php";
+	      
+        	$menu['links']['add'] = "/plugins/tasklist/front/list.form.php";
 
-     	return $menu;
-   }
-
-
+	     	return $menu;
+	}
 }
